322e74d5a794ab8922fa0606491432056ec956e6
Thomas Moon <tmoon@linux-a2.ews.illinois.edu>
2020-07-17 13:02:35 -0500

    lec4-ex1

---
+++ 05reverse-stack/test1.asm
@@ -0,0 +1,12 @@
+        .ORIG       x4000
+        .FILL       #48
+        .FILL       #49
+        .FILL       #50
+        .FILL       #51
+        .FILL       #52
+        .FILL       #53
+        .FILL       #54
+        .FILL       #55
+        .FILL       #56
+        .FILL       #57
+        .END
